169bcc0ae18d3cdbd3d98915bcf9bdcaa1f7bbb5
Sergio González <sgzzsifuentes@gmail.com>
2024-04-29 18:30:02 -0600

    Testing managing states

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,32 @@
-function ListGroup() {
-  return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
-  );
+import { useState } from "react";
+
+function ListGroup() 
+{
+	let items = 
+    [
+		"New York",
+		"San Francisco",
+		"Los Angeles",
+		"Florida",
+		"Tokyo",
+	];	
+
+	// Hook
+	const [selectedIndex, setSelectedIndex] = useState(-1);
+
+	return (
+		<>
+			<h1>List</h1>
+            { items.length === 0 && <p>No item found</p>}
+			<ul className="list-group">
+				{items.map((item, index) => (
+					<li className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' } key={item} onClick={ () => { setSelectedIndex (index); } }>
+						{item}
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
 export default ListGroup;
